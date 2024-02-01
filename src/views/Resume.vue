ca7f4309e65363aebab84ecf412e6172fc050cb5
EdReckard <98890435+EdReckard@users.noreply.github.com>
2024-01-31 19:39:55 -0500

    Update Resume.vue

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <SingleBlock header="Welcome to the Resume Page!" 
-    text="Fill in text here!"/>
+    text="Below you can see my resume and a link below to download"/>
+    <img src="../assets/Ed_Reckard_Resume(January 2024).png"/>
+    <a href="https://drive.google.com/file/d/1ylBDW12jWk2JvOGy3bSkj1G8bbGM7p5R/view?usp=sharing" target="_blank" rel="noopener">Click here to download</a>.
 
   </div>
 </template>
@@ -9,11 +11,15 @@
 <script>
 /* eslint-disable */
 import SingleBlock from './../components/SingleBlock.vue';
+
+
 export default {
   name: 'Resume',
   components: {
     SingleBlock,
+    
   
   },
+  
 }
 </script>
