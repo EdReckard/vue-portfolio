345ddda81081d654b59b65975bdd2d58295f36d2
EdReckard <98890435+EdReckard@users.noreply.github.com>
2024-01-31 22:50:23 -0500

    whitescreen fix

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,24 @@
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 0px;
+  background-color: rgb(223, 211, 147);
+  position:static;
+  z-index:1;
+  top:0;
+  left:0;
+  
+  bottom:0;
+  padding:1em;
+  border-radius: 0.2em;
+  flex-direction: column;
+  transition: 0.9s ease;
+}
+</style>
+
 <template>
 
 <!-- 
@@ -38,25 +59,6 @@ export default {
 </script>
 
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin: 0px;
-  background-color: rgb(223, 211, 147);
-  position:static;
-  z-index:1;
-  top:0;
-  left:0;
-  
-  bottom:0;
-  padding:1em;
-  border-radius: 0.2em;
-  flex-direction: column;
-  transition: 0.9s ease;
-}
-</style>
+
 
 
