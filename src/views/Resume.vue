73b6ce122cc4ba510dd32cffb36082aa480e7a7d
EdReckard <98890435+EdReckard@users.noreply.github.com>
2024-02-29 15:39:58 -0500

    resume change

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -3,7 +3,7 @@
     <SingleBlock header="Resume" 
     text="Below you can see my resume. Click the link below to download it."/>
     <div class="resume">
-      <img src="./../assets/Ed_Reckard_Resume(January 2023).png" width="900" height="1200"/>
+      <img src="./../assets/Ed_Reckard_Resume(Feburary 2023).png" width="900" height="1200"/>
       <br>
       <a href="https://drive.google.com/file/d/1ylBDW12jWk2JvOGy3bSkj1G8bbGM7p5R/view?usp=sharing" target="_blank" rel="noopener">Click here to download</a>.
     </div>
